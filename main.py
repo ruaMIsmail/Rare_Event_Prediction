a0ff18374f8445e9c4511a126111bfda9965e6e9
ruaMIsmail <roaa.abuluka@gmail.com>
2023-08-22 09:38:59 +0200

    add train, main and model

---
+++ main.py
@@ -0,0 +1,17 @@
+from train import AutoencoderClassifier
+from model import HyperParameter
+import pandas as pd 
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve, f1_score
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable as V
+
+
+params = HyperParameter()
+
+csv_file_path = 'Base.csv'
+train  = AutoencoderClassifier(csv_file_path, params.device, params.batch_size, params.epochs, params.encoding_dim)
